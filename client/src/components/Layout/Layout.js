2e2d7cc22e3c2e7b349a891026e78a8f4d9d47f2
georgifotev1 <georgifotev12@gmail.com>
2023-03-12 13:23:33 +0200

    Added a debounce hook

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Home } from "../../sections/Home/Home";
 import { Login } from "../../sections/Login/Login";
 import { Register } from "../../sections/Register/Register";
+
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 
 export const Layout = () => {
-  const [isLogged, setIsLogged] = useState(false);
   return (
     <BrowserRouter>
       <Header />
